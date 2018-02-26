8862bb3f87f5d3407f79371f963271152c5ca172
Matthew Morgan <matt@dulcetsoftware.com>
2018-02-26 06:57:18 -0500

    Change update for ComputeCell

--- react.js
+++ react.js
@@ -1,4 +1,4 @@
-class StatefulCell {
+class NotifyingCell {
   constructor() {
     this.subscribers = []
   }
@@ -12,7 +12,7 @@ class StatefulCell {
   }
 }
 
-class InputCell extends StatefulCell {
+class InputCell extends NotifyingCell {
   constructor(value) {
     super()
     this.value = value
@@ -35,28 +35,30 @@ class InputCell extends StatefulCell {
 }
 
 
-class ComputeCell extends StatefulCell {
+class ComputeCell extends NotifyingCell {
   constructor(inputCells, fn) {
     super()
     this.fn = fn
     this.inputCells = inputCells
     this.inputCells.forEach(cell => cell.addSubscriber(this))
-    this.getValue()
+    this.value = this.fn(this.inputCells)
     this.callbacks = []
   }
 
+  // this cell's values are computed on request
   getValue() {
-    this.value = this.fn(this.inputCells)
+    this.updateValue()
     return this.value
   }
 
-  updateValue() {
+  updateValue(){
     let newValue = this.fn(this.inputCells)
     if (newValue !== this.value) {
-      this.getValue()
+      this.value = newValue
       this.notify()
       this.runCallbacks()
     }
+
   }
 
   runCallbacks() {
